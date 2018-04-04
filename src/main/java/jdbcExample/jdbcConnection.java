cb50f6853301ade0a535e0a875d15e49913b33fe
jeffpascoe <jeffpascoe9@gmail.com>
2018-04-03 21:17:35 -0500

    Using JDBC to connect to a local MYSQL database.

--- src/main/java/jdbcExample/jdbcConnection.java
+++ src/main/java/jdbcExample/jdbcConnection.java
@@ -1,9 +1,31 @@
 package jdbcExample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class jdbcConnection {
 
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String JDBC_DB_URL = "jdbc:mysql://localhost:3306/db_example";
+
+	static final String JDBC_USER = "";
+	static final String JDBC_PASS = "";
+	
 	public static void main(String[] args) {
-		System.out.println("hello");
+		try {
+			Class.forName(JDBC_DRIVER);
+			Connection conn = DriverManager.getConnection(JDBC_DB_URL, JDBC_USER, JDBC_PASS);
+			
+			PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM user_table");
+			
+			ResultSet rs = preparedStatement.executeQuery();
+			System.out.println(rs.first());
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
