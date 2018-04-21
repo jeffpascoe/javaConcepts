5978ff296a2fe3e5d49831c5c8efc91a2fccce16
jeffpascoe <jeffpascoe9@gmail.com>
2018-04-21 10:49:33 -0500

    Adding one class that checks if input is a palindrome and another that checks if input is prime.

---
+++ src/main/java/AssortedProblems/PalindromeIterative.java
@@ -0,0 +1,19 @@
+package AssortedProblems;
+
+public class PalindromeIterative {
+
+	public static void main(String[] args) {
+		System.err.println(palindromeChecker("raceCar"));
+	}
+	
+	public static boolean palindromeChecker(String inputString) {
+		StringBuilder sb = new StringBuilder(inputString.toLowerCase());	
+		
+		if(inputString.toLowerCase().contentEquals(sb.reverse())) {
+			return true;
+		}
+	
+		return false;
+	}
+
+}
