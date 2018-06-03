f7d38db58d67446af2ad8a48f59f7db61bb9e6d1
jeffpascoe <jeffpascoe9@gmail.com>
2018-06-02 20:25:03 -0500

    Merge sort implementation.

--- src/main/java/AssortedProblems/MergeSort.java
+++ src/main/java/AssortedProblems/MergeSort.java
@@ -1,39 +1,62 @@
 package AssortedProblems;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Random;
 
 
 
 public class MergeSort {
 
 	public static void main(String[] args) {
-		int[] intArray = new int[] { 1,6,7,8,2,3,4,5,8,9,4,5,109 };
+		int[] arr = new int[10];
+		Random rand = new Random();
+		for(int i=0; i < 10; i++) { arr[i] = rand.nextInt(10); }
 		
-		mergeSort(intArray, 0, intArray.length);
+		mergeSort(arr, 0, arr.length-1);
 	}
 	
-	public static void mergeSort(int[] list, int lowIndex, int highIndex) {
-		if(lowIndex == highIndex) {
-			return;
-		} else {
-			System.out.println(lowIndex);
-			System.out.println(highIndex);
-			
-			int midIndex = (lowIndex + highIndex)/2;
-			mergeSort(list, lowIndex, midIndex);
-			mergeSort(list, midIndex + 1, highIndex);
-			System.out.println(lowIndex);
-			System.out.println( midIndex + 1);
-			System.out.println(lowIndex);
-//			merge(list, lowIndex, midIndex + 1, highIndex);
+	public static void mergeSort(int[] arr, int startIndex, int endIndex) {
+		if(startIndex <= endIndex) {
+			int midIndex = (arr.length-1)/2;
+			mergeSort(arr, startIndex, midIndex);
+			mergeSort(arr, midIndex, endIndex);
+			merge(arr, startIndex, midIndex, endIndex);
 		}
 	}
 	
-//	public void merge(int[] list, int lowIndex, int midIndex, int highIndex) {
-//		int
-//	}
-//	
-	
-
+	public static void merge(int[] arr, int startIndex, int midIndex, int endIndex) {
+		int leftSubSize = midIndex - startIndex -1;
+		int rightSubSize = leftSubSize+1;
+		int leftSubIndex = 0;
+		int rightSubIndex = 0;
+		int newSubArrayIndex = 0;
+		
+		int[] leftSubArray = new int[leftSubSize];
+		int[] rightSubArray = new int[rightSubSize];
+		
+		for(int i = startIndex; i <= leftSubSize; i++ ) leftSubArray[i] = arr[i+1];
+		for(int j = midIndex; j <= rightSubSize; j++) rightSubArray[j] = arr[midIndex + j+1];
+		
+		while(leftSubIndex < leftSubSize && rightSubIndex < rightSubIndex) {
+			if(arr[leftSubIndex] <= arr[rightSubIndex]) {
+				arr[newSubArrayIndex] = leftSubArray[leftSubIndex];
+				leftSubIndex++;
+			} else {
+				arr[newSubArrayIndex] = rightSubArray[rightSubIndex];
+				rightSubIndex++;
+			}
+			newSubArrayIndex++;
+		}
+		
+		while(leftSubIndex < leftSubSize) {
+			arr[newSubArrayIndex] = leftSubArray[leftSubIndex];
+			leftSubIndex++;
+			newSubArrayIndex++;
+		}
+		while(rightSubIndex < rightSubSize) {
+			arr[newSubArrayIndex] = rightSubArray[rightSubIndex];
+			rightSubIndex++;
+			newSubArrayIndex++;
+		}
+	}
 }
