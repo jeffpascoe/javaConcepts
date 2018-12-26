38de67e0df2fc3dfc0c489d3f86f98704afee769
jeffpascoe <jeffpascoe9@gmail.com>
2018-12-25 21:36:54 -0600

    Implementation of insertion sort.

--- src/main/java/sortingAlgorithms/InsertionSort.java
+++ src/main/java/sortingAlgorithms/InsertionSort.java
@@ -1,11 +1,32 @@
 package sortingAlgorithms;
 
+import java.util.Arrays;
+
 public class InsertionSort {
 
 	public static void main(String[] args) {
-		
+		Double[] dubs = new Double[100];
+		for (int i = 0; i < 100; i++) {
+			dubs[i] = Math.random();
+		}
 
+		Arrays.stream(dubs).forEach(System.out::println);
+		sort(dubs);
+		Arrays.stream(dubs).forEach(System.out::println);
 	}
 
+	public static <T extends Comparable> T[] sort(T[] array) {
+		T temp;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i; j > 0 ; j--) {
+				if(array[j].compareTo(array[j-1]) < 0) {
+					temp = array[j];
+					array[j] = array[j-1];
+					array[j-1] = temp;
+				}
+			}
+		}
 
+		return array;
+	}
 }
